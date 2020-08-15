ce74983b1f1c0d3e41ddeb61495d6ebe07337fe7
unknown <ibairva@gmail.com>
2020-08-15 18:53:58 +0530

    Updates

---
+++ precache-manifest.fa795ee9645177a351a5d16954c1a6bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6119930c396d12aae5fbe09687cc083b",
+    "url": "/myreactlogin/index.html"
+  },
+  {
+    "revision": "fd1c0ebb0975a394fcc2",
+    "url": "/myreactlogin/static/css/main.56b32afd.chunk.css"
+  },
+  {
+    "revision": "97ce40be21c4efc12cbe",
+    "url": "/myreactlogin/static/js/2.0b88921e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreactlogin/static/js/2.0b88921e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1c0ebb0975a394fcc2",
+    "url": "/myreactlogin/static/js/main.a95c53ad.chunk.js"
+  },
+  {
+    "revision": "928ea1c7f883ad1f9e1e",
+    "url": "/myreactlogin/static/js/runtime-main.27fedbb0.js"
+  }
+]);
